075b5bdd05e7c7d86d306447b7773a6f0d9e6b53
wate <110754234+WateTaks@users.noreply.github.com>
2025-10-12 23:38:47 -0300

    feat: adds top10 ranking for each desafio available

--- symcomp-server/desafio/urls.py
+++ symcomp-server/desafio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from desafio.views import responder_questao
+from desafio.views import responder_questao, obter_ranking
 
 urlpatterns = [
     path('questoes/<int:questao_id>/responder/', responder_questao, name='responder-questao'),
+    path('<int:desafio_id>/ranking/', obter_ranking, name='obter-ranking'), 
 ]
