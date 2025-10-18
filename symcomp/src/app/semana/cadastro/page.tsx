256c4fbc3b2b8538df363f2b9cb4efd843e742bf
senshiacy <uchiyama@usp.br>
2025-10-18 11:00:41 -0300

    fix: Fixing Background

--- symcomp/src/app/semana/cadastro/page.tsx
+++ symcomp/src/app/semana/cadastro/page.tsx
@@ -25,6 +25,8 @@ import { SCButton } from '@/components/sc-2025/button'
 import { TypographyH1 } from '@/components/sc-2025/typography'
 import { TypographyH2 } from '@/components/sc-2025/typography'
 import { useState } from 'react'
+import SemanaHeader from '../header'
+
 
 // Representa os campos do formulário.
 const formSchema = z.object({
@@ -70,81 +72,86 @@ export default function CadastroPage() {
   }
 
   return (
-    <div className="max-w-5xl mx-auto p-6 grid gap-6 bg-red-500">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="p-6 grid gap-4"
-        >
+    <div className='w-full flex justify-center bg-sc-2025-background min-h-svh'>
+      <div className="max-w-[1024px] w-full">
+        <SemanaHeader></SemanaHeader>
+        <main className="max-w-5xl mx-auto p-6 grid gap-6">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="p-6 grid gap-4"
+            >
 
-          <TypographyH1>Cadastrar</TypographyH1>
-          <div className='text-[#0E0A47]'>
-            <TypographyH2>Preencha suas informações para concluir o cadastro:</TypographyH2>
-          </div>
+              <TypographyH1>Cadastrar</TypographyH1>
+              <div className='text-[#0E0A47]'>
+                <TypographyH2>Preencha suas informações para concluir o cadastro:</TypographyH2>
+              </div>
 
-          <div className='text-[#0E0A47]'>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <SCField>
-                  <SCLabel>Nome</SCLabel>
-                  <SCInput placeholder="Ex.: Grace Hopper" {...field} required></SCInput>
-                  {
-                    errors.name && errors.name.map((e : string, index : number) => {
-                      return (
-                        <TypographyH2 key={index}>{e}</TypographyH2>
-                      )
-                    })
-                  }
-                </SCField>
-              )}
-            />
-          </div>
+              <div className='text-[#0E0A47]'>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <SCField>
+                      <SCLabel>Nome</SCLabel>
+                      <SCInput placeholder="Ex.: Grace Hopper" {...field} required></SCInput>
+                      {
+                        errors.name && errors.name.map((e : string, index : number) => {
+                          return (
+                            <TypographyH2 key={index}>{e}</TypographyH2>
+                          )
+                        })
+                      }
+                    </SCField>
+                  )}
+                />
+              </div>
 
-          <div className='text-[#0E0A47]'>
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <SCField>
-                  <SCLabel>Email</SCLabel>
-                  <SCInput placeholder="Ex.: grace.hopper@ime.usp.br" {...field} required></SCInput>
-                  {
-                    errors.email && errors.email.map((e : string, index : number) => {
-                      return (
-                        <TypographyH2 key={index}>{e}</TypographyH2>
-                      )
-                    })
-                  }
-                </SCField>
-              )}
-            />
-          </div>
+              <div className='text-[#0E0A47]'>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <SCField>
+                      <SCLabel>Email</SCLabel>
+                      <SCInput placeholder="Ex.: grace.hopper@ime.usp.br" {...field} required></SCInput>
+                      {
+                        errors.email && errors.email.map((e : string, index : number) => {
+                          return (
+                            <TypographyH2 key={index}>{e}</TypographyH2>
+                          )
+                        })
+                      }
+                    </SCField>
+                  )}
+                />
+              </div>
 
-          <div className="text-[#0E0A47]">
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <SCField>
-                  <SCLabel>Senha</SCLabel>
-                  <SCInput placeholder="Ex.: &S3m4n4D@C0mpUT4c@0" {...field} required type='password'></SCInput>
-                  {
-                    errors.password && errors.password.map((e : string, index : number) => {
-                      return (
-                        <TypographyH2 key={index}>{e}</TypographyH2>
-                      )
-                    })
-                  }
-                </SCField>
-              )}
-            />
-          </div>
+              <div className="text-[#0E0A47]">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <SCField>
+                      <SCLabel>Senha</SCLabel>
+                      <SCInput placeholder="Ex.: &S3m4n4D@C0mpUT4c@0" {...field} required type='password'></SCInput>
+                      {
+                        errors.password && errors.password.map((e : string, index : number) => {
+                          return (
+                            <TypographyH2 key={index}>{e}</TypographyH2>
+                          )
+                        })
+                      }
+                    </SCField>
+                  )}
+                />
+              </div>
 
-          <SCButton type='submit'>Finalizar</SCButton>
-        </form>
-      </Form>
+              <SCButton type='submit'>Finalizar</SCButton>
+            </form>
+          </Form>
+        </main>
+      </div>
     </div>
   )
 }
