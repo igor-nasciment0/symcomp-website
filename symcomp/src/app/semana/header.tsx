04756f6393290eb9186faf643adbbeb784866497
Xnths <jonathascastilhoes@gmail.com>
2025-10-18 02:04:34 -0300

    feat: adding navigation menu

--- symcomp/src/app/semana/header.tsx
+++ symcomp/src/app/semana/header.tsx
@@ -1,12 +1,17 @@
 'use client'
 
+import { Text } from '@/components/sc-2025/typography'
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from '@/components/ui/sheet'
-import { semanaLogin } from '@/lib/routes'
-import { Menu } from 'lucide-react'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
+import { semanaCrongorama, semanaHome, semanaLogin } from '@/lib/routes'
+import { cn } from '@/lib/utils'
+import { LogIn, Menu } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function SemanaHeader() {
+  const { data } = useCurrentUser()
+
   return (
     <div className="p-4 w-full flex flex-row justify-between items-start bg-transparent">
       <Image src="/sc-2025/ime-usp-branca.svg" alt="" width={52} height={59} />
@@ -21,9 +26,23 @@ export default function SemanaHeader() {
         <SheetTrigger asChild>
           <Menu size={32} />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>Olá,</SheetHeader>
-          <Link href={semanaLogin}>Login</Link>
+        <SheetContent className="bg-sc-2025-contrast border-none">
+          <div className="flex flex-row gap-4 items-center justify-center">
+            <Link href={semanaLogin} className="bg-white w-fit p-2">
+              <Text>Entrar</Text>
+            </Link>
+            <Link href={semanaLogin} className="bg-sc-2025-primary w-fit p-2">
+              <Text>Cadastrar</Text>
+            </Link>
+          </div>
+          <div className="flex flex-col gap-2 mt-8">
+            <Link href={semanaHome}>
+              <Text className="text-sc-2025-background text-xl">Página inicial</Text>
+            </Link>
+            <Link href={semanaCrongorama}>
+              <Text className="text-sc-2025-secondary text-xl">Cronograma</Text>
+            </Link>
+          </div>
         </SheetContent>
       </Sheet>
     </div>
