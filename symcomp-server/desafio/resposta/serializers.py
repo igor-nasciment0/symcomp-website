832c708be6ad394fe6cbb0fbee24eb33d5e899e6
wate <110754234+WateTaks@users.noreply.github.com>
2025-10-19 02:27:08 -0300

    nem sei qq deu

--- symcomp-server/desafio/resposta/serializers.py
+++ symcomp-server/desafio/resposta/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Resposta
-from desafios.questao.models import Questao
+from desafio.questao.models import Questao
 
 class RespostaSerializer(serializers.ModelSerializer):
     questao = serializers.PrimaryKeyRelatedField(
--- symcomp-server/desafio/urls.py
+++ symcomp-server/desafio/urls.py
@@ -4,6 +4,7 @@ from desafio.views import responder_questao, obter_ranking, salvar_resposta, sub
 urlpatterns = [
     #path('questoes/<int:questao_id>/responder/', responder_questao, name='responder-questao'),
     path('<int:desafio_id>/ranking/', obter_ranking, name='obter-ranking'), 
+    # O formato para resposta deve ser algo como {'questao': ID, 'resposta': 'resposta do usuario '}
     path('respostas/salvar/', salvar_resposta, name='salvar-resposta'),
     path('<int:desafio_id>/submeter', submeter_formulario, name='submeter-formulario'),
 ]
