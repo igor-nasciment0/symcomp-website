875b41df472c300742f90b377633043f8decff62
Mateus Cruzatto <cruzatto@proton.me>
2026-08-08 16:15:45 -0300

    feat: add app lifespan to dispose database engine

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import Depends, FastAPI
 
 from app.api.router import api_router
-from app.core import config
+from app.core import config, database
 
-app = FastAPI(title="SymComp API", version="0.1.0")
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    yield
+    await database.engine.dispose()
+
+
+app = FastAPI(title="SymComp API", version="0.1.0", lifespan=lifespan)
 app.include_router(
-    api_router, prefix="/api/v1", dependencies=[Depends(config.get_settings)]
+    api_router,
+    prefix="/api/v1",
+    dependencies=[Depends(config.get_settings), Depends(database.get_session)],
 )
