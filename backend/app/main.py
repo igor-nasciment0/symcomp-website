36032276a1e059299abb126b2c971ccef41ca7b7
Mateus Cruzatto <cruzatto@proton.me>
2026-08-08 23:19:18 -0300

    feat: configure loggin in main.py and add log_request middleware to app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import Depends, FastAPI
 
+from app.api.middlewares import log_request
 from app.api.router import api_router
 from app.core import config
 
 app = FastAPI(title="SymComp API", version="0.1.0")
+
+app.middleware("http")(log_request)
+
 app.include_router(
     api_router, prefix="/api/v1", dependencies=[Depends(config.get_settings)]
 )
